d5a6f686b4ba1c9a5ec5f0d7640ec26604de6e6e
kawagh <kawagh@example.com>
2022-07-17 16:25:04 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# singlue
+A tool to resolve function,class codes and integrate them to single file.
+
+This program depends on `ast.unparse()`(added in `python3.9`).
+
+## example
+```sh
+python main.py example.py
+python main.py example2.py
+```
---
+++ singlue/main.py
@@ -0,0 +1,50 @@
+import ast
+import sys
+import argparse
+from pathlib import Path
+from typing import Optional
+
+
+def resolve_fn_or_cls(name: str, src_ast: ast.Module) -> Optional[ast.stmt]:
+    found_fn_or_cls = next(
+        filter(lambda x: getattr(x, "name") == name, src_ast.body), None
+    )
+    return found_fn_or_cls
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", help="file to glue")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    print("====================================", file=sys.stderr)
+    assert Path(args.source).exists()
+    with open(Path(args.source)) as f:
+        res = ast.parse(source=f.read())
+    for stmt in res.body:
+        print(ast.unparse(stmt), file=sys.stderr)
+    print("====================================", file=sys.stderr)
+
+    for stmt in res.body:
+        if isinstance(stmt, ast.ImportFrom):
+            # TODO replace import part to resolved code
+            # TODO skip third party library
+            for func_or_cls in stmt.names:
+                # TODO fix source directory to search module
+                with open(Path(__file__).parent / f"{stmt.module}.py") as f:
+                    res = ast.parse(source=f.read())
+                    found_fn_or_cls = resolve_fn_or_cls(func_or_cls.name, res)
+                    if found_fn_or_cls:
+                        print(ast.unparse(found_fn_or_cls))
+                    else:
+                        raise RuntimeError(f"cannot find {func_or_cls.name}")
+        else:
+            print(ast.unparse(stmt))
+    # TODO resolve,duplicate import
+
+
+if __name__ == "__main__":
+    main()
